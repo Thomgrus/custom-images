50bd2e59ab4ab87f1396e791e558900e50e7225e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-12 18:19:10 +0000

    Adding tarfile member sanitization to extractall()

--- backuper/backuper.py
+++ backuper/backuper.py
@@ -22,7 +22,26 @@ def process_metadata(target_path):
 
 def uncompress_tarfile(input_filename, target_dir):
     with tarfile.open(input_filename, "r:gz") as tar:
-        tar.extractall(path=target_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=target_dir)
 
 def ftp_restore(args, metadata):
     ftp_config = process_ftp_config_file(args.ftp_file)
